47d414b1a52ab51ab3c560e7b083ab7858dc1981
Naeyaya <natnaeleyaya@gmail.com>
2022-09-08 13:46:38 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,10 +8,9 @@
 
 /**
  *  * main - Prints "with proper grammar, but the outcome is
- *   *                a piece of art," followed by a new line.
- *    *
- *     * Return: Always 0.
- *      */
+ *                a piece of art," followed by a new line.
+ *Return: Always 0.
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
