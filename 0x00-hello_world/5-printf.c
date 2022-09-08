ea97142113631d391ffceb5f83de6c12ca1db5ca
Naeyaya <natnaeleyaya@gmail.com>
2022-09-08 13:05:46 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 /*
- * Program print usng printf function
+ * Program print usng 
+ * printf function
  */
 #include <stdio.h>
 int main(void)
 {
-/*                                                                                                                                       * Program print usng printf function                                                                                                    */
+/*                                                                                                                                       * Program print usng 
+ * printf function
+ */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
