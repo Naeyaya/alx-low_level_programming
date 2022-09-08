1cbb521bbb7077eee208bfd9bdcfd88eb1dd8abb
Naeyaya <natnaeleyaya@gmail.com>
2022-09-08 13:36:41 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,12 @@
  *Program prints usng printf function
  */
 #include <stdio.h>
+/**                                                                                                                                     
+ *  * main - Prints "Programming is like building a multilingual                                                                           
+ *   * puzzle, followed by a new line.                                                                                                      
+ *    * Return: Always 0.                                                                                                                    
+ *     */    
 int main(void)
-	/*Printer*/
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
