b5fd906922d9124535a891a920b2082c9c5dae73
Naeyaya <natnaeleyaya@gmail.com>
2022-09-08 13:19:56 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,7 @@
  *Program prints usng printf function
  */
 #include <stdio.h>
-/*Program prints                                                                                                                        
- *printf function*/  
+/*Program prints printf function*/  
 int main(void)
 {
 
