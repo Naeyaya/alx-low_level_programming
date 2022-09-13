08497c3d0991a6e1c2a26214641e285227c6d590
Naeyaya <natnaeleyaya@gmail.com>
2022-09-13 14:14:44 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,17 @@
-#include"main.h"
-/*
- *  print_alphabet - entry point
- *  Description - prints small letter alphabets
- *  Return: ALways 0
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
  */
 void print_alphabet(void)
 {
-	char c;
-	int i = 0;
+		char c;
 
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
-		i++;
-	}
+			for (c = 'a'; c <= 'z'; c++)
+					{
+								_putchar(c);
+									}
+
+				_putchar('\n');
 }
